859d688d80f2670291075b481c73a43bcd9667db
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-07-12 11:11:39 -0400

    Automatic update.

--- test/api.js
+++ test/api.js
@@ -1,5 +1,5 @@
 /**
- * Object States API
+ * Object Channels API
  *
  * mocha test/api.js --reporter list --ui exports --watch
  *
@@ -9,35 +9,69 @@
  */
 module.exports = {
 
-  /**
-   * Prepare Environment
-   *
-   */
-  'before': function() {
-
-    // Dependancies
-    require( 'should' );
-
+  before: function() {
+    module.noop = function() {}
   },
 
-  'object-states': {
+  'Object Channels API': {
 
     'has expected methods': function() {
-      var states = require( 'object-states' );
+      var channels = require( '../' );
 
-      states.should.have.property( 'create' );
-      states.should.have.property( 'prototype' );
-      states.should.have.property( 'utility' );
+      // Constructor tests
+      channels.should.be.a( 'function' );
+      channels.should.have.property( 'noop' );
+      channels.should.have.property( 'prototype' );
 
+      // Inherited Abstract methods
+      channels.should.have.property( 'create' );
+      channels.should.have.property( 'use' );
+      channels.should.have.property( 'get' );
+      channels.should.have.property( 'set' );
+      channels.prototype.should.have.property( 'mixin' );
+      channels.prototype.should.have.property( 'use' );
+
+      // Prototypal Methods
+      channels.prototype.should.have.property( 'on' );
+      channels.prototype.should.have.property( 'onAny' );
+
+    },
+
+    'can be instantiated via create() method.': function() {
+      var instance = require( '../' ).create();
+
+      instance.should.have.property( 'mixin' );
+      instance.should.have.property( 'on' );
+      instance.should.have.property( 'onAny' );
+      instance.should.have.property( 'off' );
+      instance.should.have.property( 'emit' );
+      instance.should.have.property( 'emit' );
+
+    },
+
+    'method': {
+
+      'on() is chainable.': function() {
+        require( '../' ).create().on( 'noop', module.noop ).should.have.property( 'emit' );
+      },
+
+      'off() is chainable.': function() {
+        //require( '../' ).create().off( 'noop', module.noop ).should.have.property( 'emit' );
+      },
+
+      'emit() is chainable.': function() {
+        //require( '../' ).create().emit( 'noop', module.noop ).should.have.property( 'emit' );
+      },
+
+      'emit() works.': function( done ) {
+        require( '../' ).create().on( 'ping', function( data ) {
+          data.should.equal( 'ding' );
+          this.should.have.property( 'event', 'ping' );
+          done()
+        }).emit( 'ping', 'ding' );
+      }
     }
 
-  },
-
-  /**
-   * Destroy Environment
-   *
-   */
-  'after': function() {
   }
 
 };
